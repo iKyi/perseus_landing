11b4d4f9159c3b7f9da87fdc981a94be7c8ea096
iky cc <ikymetaverse@gmail.com>
2022-05-09 21:22:26 +0300

    + added formspree functionality

--- src/components/Reusable/ContactBox.tsx
+++ src/components/Reusable/ContactBox.tsx
@@ -1,13 +1,15 @@
-import { Box, Grid, Button } from "@mui/material";
+import { Box, Grid, Button, Alert } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { SxProps } from "@mui/material";
 import SectionHeader from "./SectionHeader";
-import contactBg from "assets/images/contactBox/background.png";
+// import contactBg from "assets/images/contactBox/background.png";
 import { sectionMarginBottom } from "constants/styleConstants";
 import ISectionHeaderStrapi from "utils/types/ISectionHeader";
+import { useForm as useFormspreeForm } from "@formspree/react";
+import { CheckCircleOutlineTwoTone } from "@mui/icons-material";
 
 const phoneRegExp =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
@@ -40,8 +42,10 @@ export type ContactBoxPropsType = {
 };
 
 const ContactBox: React.FC<ContactBoxPropsType> = ({ sx, header }) => {
+  const [state, handleSubmit] = useFormspreeForm("moqryvpb");
+
   const {
-    handleSubmit,
+    handleSubmit: hookFormSubmit,
     control,
     formState: { errors },
   } = useForm<FormValues>({
@@ -54,7 +58,11 @@ const ContactBox: React.FC<ContactBoxPropsType> = ({ sx, header }) => {
     },
   });
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    handleSubmit(data);
+  };
+
+  const { succeeded } = state;
 
   // *************** RENDER *************** //
   return (
@@ -62,8 +70,9 @@ const ContactBox: React.FC<ContactBoxPropsType> = ({ sx, header }) => {
       sx={{
         px: [5, 5, 12],
         py: [3, 3, 7],
-        background: `rgba(0,0,0,0.1), url('${contactBg}')`,
-        backgroundBlendMode: "darken",
+        // background: `url('${contactBg}'), rgba(0,0,0,0.3)`,
+        backgroundImage:
+          "linear-gradient(30.91deg, rgba(143, 60, 221, 0.10) 1.01%, rgba(0,0,0,0.3) 100%)",
         border: "1px solid",
         borderImageSlice: 1,
         borderImageSource:
@@ -75,93 +84,118 @@ const ContactBox: React.FC<ContactBoxPropsType> = ({ sx, header }) => {
       id="contact"
     >
       <SectionHeader {...header} />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={[2, 2, 4, 4]}>
-          <Grid item xs={12} sm={12} md={4}>
-            <Controller
-              name={"name"}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  fullWidth
-                  label={"Name"}
-                  InputProps={{ ...field }}
-                  error={errors.name ? true : false}
-                  onBlur={field.onBlur}
-                  helperText={<>{errors.name ? errors.name.message : ""}</>}
-                />
-              )}
-            />
+      {succeeded && (
+        <Alert
+          variant="outlined"
+          severity="success"
+          sx={{
+            fontSize: "1.4rem",
+          }}
+          icon={
+            <Box
+              sx={{
+                fontSize: "1.8rem",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <CheckCircleOutlineTwoTone fontSize="inherit" />
+            </Box>
+          }
+        >
+          <Box sx={{ display: "flex", alignItems: "center" }}></Box>
+          Message sent!
+        </Alert>
+      )}
+      {!succeeded && (
+        <form onSubmit={hookFormSubmit(onSubmit)}>
+          <Grid container spacing={[2, 2, 4, 4]}>
+            <Grid item xs={12} sm={12} md={4}>
+              <Controller
+                name={"name"}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    fullWidth
+                    label={"Name"}
+                    InputProps={{ ...field }}
+                    error={errors.name ? true : false}
+                    onBlur={field.onBlur}
+                    helperText={<>{errors.name ? errors.name.message : ""}</>}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <Controller
+                name={"phone"}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    fullWidth
+                    label={"Phone"}
+                    InputProps={{ ...field }}
+                    error={errors.phone ? true : false}
+                    onBlur={field.onBlur}
+                    helperText={<>{errors.phone ? errors.phone.message : ""}</>}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <Controller
+                name={"email"}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    fullWidth
+                    label={"Email address"}
+                    InputProps={{ ...field }}
+                    error={errors.email ? true : false}
+                    onBlur={field.onBlur}
+                    helperText={<>{errors.email ? errors.email.message : ""}</>}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              <Controller
+                name={"message"}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    fullWidth
+                    label={"Message"}
+                    InputProps={{ ...field }}
+                    error={errors.message ? true : false}
+                    onBlur={field.onBlur}
+                    multiline
+                    minRows={3}
+                    helperText={
+                      <>{errors.message ? errors.message.message : ""}</>
+                    }
+                  />
+                )}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <Controller
-              name={"phone"}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  fullWidth
-                  label={"Phone"}
-                  InputProps={{ ...field }}
-                  error={errors.phone ? true : false}
-                  onBlur={field.onBlur}
-                  helperText={<>{errors.phone ? errors.phone.message : ""}</>}
-                />
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <Controller
-              name={"email"}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  fullWidth
-                  label={"Email address"}
-                  InputProps={{ ...field }}
-                  error={errors.email ? true : false}
-                  onBlur={field.onBlur}
-                  helperText={<>{errors.email ? errors.email.message : ""}</>}
-                />
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Controller
-              name={"message"}
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  fullWidth
-                  label={"Message"}
-                  InputProps={{ ...field }}
-                  error={errors.message ? true : false}
-                  onBlur={field.onBlur}
-                  multiline
-                  minRows={3}
-                  helperText={
-                    <>{errors.message ? errors.message.message : ""}</>
-                  }
-                />
-              )}
-            />
-          </Grid>
-        </Grid>
-        <Box sx={{ textAlign: "center", mt: [2, 2, 5] }}>
-          <Button
-            type="submit"
-            size="large"
-            variant="outlined"
-            color="secondary"
-            sx={{
-              width: 220,
-              maxWidth: "100%",
-              height: 52,
-            }}
-          >
-            Contact Us
-          </Button>
-        </Box>
-      </form>
+          <Box sx={{ textAlign: "center", mt: [2, 2, 5] }}>
+            <Button
+              type="submit"
+              size="large"
+              variant="outlined"
+              color="secondary"
+              sx={{
+                width: 220,
+                maxWidth: "100%",
+                height: 52,
+              }}
+            >
+              Contact Us
+            </Button>
+          </Box>
+        </form>
+      )}
     </Box>
   );
 };
